d366ca0feed9586a199708e01399b7a9df25272a
Henrik Panhans <henrik@panhans.dev>
2024-04-18 11:12:12 +0200

    Fix documentation deploy
    Signed-off-by: Henrik Panhans <henrik@panhans.dev>

--- Package.swift
+++ Package.swift
@@ -23,7 +23,6 @@ let package = Package(
         .package(url: "https://github.com/apple/swift-argument-parser", from: "1.0.0"),
         .package(url: "https://github.com/henrik-dmg/CLIFoundation", branch: "main"),
         .package(url: "https://github.com/ChimeHQ/GlobPattern", branch: "main"),
-        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.1.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
